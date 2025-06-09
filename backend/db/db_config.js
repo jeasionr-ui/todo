05638726d26343baddaf45c6b330e6f0a626239d
joe <jeasionr@foxmail.com>
2025-06-09 11:58:09 +0800

    feat: 更新数据库配置，恢复本地连接设置并移除注释的远程配置

--- backend/db/db_config.js
+++ backend/db/db_config.js
@@ -1,12 +1,3 @@
-// 数据库连接配置
-// export const dbConfig = {
-//   host: '47.86.237.178',
-//   port: 3308,
-//   user: 'todo', // 如有变动请修改
-//   password: 'fXWZGS2m2JKREaFH',
-//   database: 'todo',
-// };
-
 export const dbConfig = {
   host: '127.0.0.1',
   port: 3306,
